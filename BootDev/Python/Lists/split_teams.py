c77985ff4354c606b0acd8f9f9a387bac2cf6671
Harold Fandino <haroldandres2014@hotmail.com>
2025-12-01 16:41:10 -0500

    Split teams practice from Boot.dev

---
+++ BootDev/Python/Lists/split_teams.py
@@ -0,0 +1,27 @@
+def split_players_into_teams(players):
+    
+    evenPlayers = players[0::2]
+    oddPlayers = players[1::2]
+
+    return evenPlayers, oddPlayers
+
+# listPlayers = [
+#     "Harry",
+#     "Hermione",
+#     "Ron",
+#     "Ginny",
+#     "Fred",
+#     "Neville",
+#     "Draco",
+#     "Luna",
+#     "Cho",
+#     "Gregory",
+#     "Lee",
+#     "Michael",
+#     "Lavender",
+#     "Frank",
+#     "Anthony",
+#     "Allan",
+# ]
+
+# split_players_into_teams(listPlayers)
